7a215301e4753d786909dca48ff686b742fc2a61
Arnaud Schloune <arnaud.schloune@gmail.com>
2016-12-30 07:27:55 +0100

    fixed COSTouchVisualizerWindow integration issue

--- Barceloneta/AppDelegate.swift
+++ Barceloneta/AppDelegate.swift
@@ -11,24 +11,26 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, COSTouchVisualizerWindowDelegate {
 
-    var window: UIWindow?
-//    var window: UIWindow? = {
-//        var customWindow = COSTouchVisualizerWindow(frame: UIScreen.main.bounds)
-//        customWindow.touchVisualizerWindowDelegate = self
-//        
-//        customWindow.fillColor = UIColor(red:0.07, green:0.73, blue:0.86, alpha:1)
-//        customWindow.strokeColor = UIColor.clear
-//        customWindow.touchAlpha = 0.4
-//        
-//        customWindow.rippleFillColor = UIColor(red:0.98, green:0.68, blue:0.22, alpha:1)
-//        customWindow.rippleStrokeColor = UIColor.clear
-//        customWindow.rippleAlpha = 0.4
-//        
-//        return customWindow
-//    }()
-
+//    var window: UIWindow?
+    internal var window: UIWindow? = {
+        let customWindow = COSTouchVisualizerWindow(frame: UIScreen.main.bounds)
+        
+        customWindow.fillColor = UIColor(red:0.07, green:0.73, blue:0.86, alpha:1)
+        customWindow.strokeColor = UIColor.clear
+        customWindow.touchAlpha = 0.4;
+        
+        customWindow.rippleFillColor = UIColor(red:0.98, green:0.68, blue:0.22, alpha:1)
+        customWindow.rippleStrokeColor = UIColor.clear
+        customWindow.touchAlpha = 0.4;
+        
+        return customWindow
+    }()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        guard let window = window as? COSTouchVisualizerWindow else {
+            return false
+        }
+        window.touchVisualizerWindowDelegate = self
         return true
     }
 
