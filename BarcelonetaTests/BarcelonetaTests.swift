ff2ac2bd92214714a0ec648bfcb235fb7ff2999b
Arnaud Schloune <arnaud.schloune@gmail.com>
2017-11-03 20:59:36 +0100

    test percentage

--- BarcelonetaTests/BarcelonetaTests.swift
+++ BarcelonetaTests/BarcelonetaTests.swift
@@ -25,12 +25,21 @@ class BarcelonetaTests: XCTestCase {
         //Then
     }
     */
-
     func testBarceloneta_ShouldCalculateCorrectPercentage() {
         //Given
+        let limit: CGFloat = 50.0
         //When
+        let prct0 = bcn?.percentage(limit: limit, constant: CGFloat(0))
+        let prct50 = bcn?.percentage(limit: limit, constant: CGFloat(25.0))
+        let prct80 = bcn?.percentage(limit: limit, constant: CGFloat(40.0))
+        let prct100 = bcn?.percentage(limit: limit, constant: CGFloat(50.0))
+        let prct120 = bcn?.percentage(limit: limit, constant: CGFloat(60.0))
         //Then
-        XCTAssertTrue(true)
+        XCTAssertEqual(prct0, 0)
+        XCTAssertEqual(prct50, 50)
+        XCTAssertEqual(prct80, 80)
+        XCTAssertEqual(prct100, 100)
+        XCTAssertEqual(prct120, 120)
     }
 
     override func tearDown() {
